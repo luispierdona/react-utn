35613ed68890ff015a9a9f1ded31a0f9c2e95c9f
Luis Matias Pierdona <luismathias.pierdona@de.adecco.net>
2021-07-29 12:14:41 +0200

    class change to className and TXT added

--- src/topbar/header.jsx
+++ src/topbar/header.jsx
@@ -7,16 +7,16 @@ const Header = () => {
     <Navbar bg="dark" variant="dark">
       <Container>
       <LinkContainer to='/'>
-        <Navbar.Brand><i class="fa fa-book" aria-hidden="true"></i> Biblioteca</Navbar.Brand>
+        <Navbar.Brand><i className="fa fa-book" aria-hidden="true"></i> Biblioteca</Navbar.Brand>
       </LinkContainer>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <LinkContainer to="/categorias">
-            <Nav.Link><i class="fa fa-bookmark" aria-hidden="true"></i> Categorias</Nav.Link>
+            <Nav.Link><i className="fa fa-bookmark" aria-hidden="true"></i> Categorias</Nav.Link>
           </LinkContainer>
           <LinkContainer to="/personas">
-            <Nav.Link><i class="fa fa-user-circle-o" aria-hidden="true"></i> Personas</Nav.Link>
+            <Nav.Link><i className="fa fa-user-circle-o" aria-hidden="true"></i> Personas</Nav.Link>
           </LinkContainer>
         </Nav>
       </Navbar.Collapse>
